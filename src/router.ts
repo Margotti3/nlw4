bbefe97ae63d8bb88da3dfd3ae62453922d3e347
Margotti3 <mmargottia@gmail.com>
2021-03-03 23:00:04 -0300

    fourth day completed

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { SurveyController } from './controllers/SurveyController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 const userController = new UserController();
+const surveyController = new SurveyController();
+const surveyUserController = new SendMailController();
 
 router.post('/users', userController.create);
+router.post('/survey', surveyController.create);
+router.get('/survey', surveyController.show);
+router.post('/sendEmail', surveyUserController.execute);
 
 export { router };
